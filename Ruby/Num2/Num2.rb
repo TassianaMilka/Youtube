5181f0c57d9b9c08de3dbf14ebae06f6fdc27e11
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-08-16 18:20:53 -0300

    1

---
+++ Ruby/Num2/Num2.rb
@@ -0,0 +1,47 @@
+
+#Exer
+#create mensage "Number you"
+# number=3.
+
+
+#Declaração da variável
+
+num=3;
+#Impimir a mensagem
+puts "Number you"
+puts  num 
+
+
+
+#Declaração da variável
+
+soma=2+5;
+#soma;
+#soma=2+5;
+
+#Imprimir o resultado
+
+puts soma
+
+#Declaração da variável
+
+mult=num*soma;
+
+
+#Imprimir o resultado
+
+puts mult
+
+
+#Faça um código que impimir o número 7, uma subtração entre 4 com 2,depois realiza  a divisão do que
+#foi imprimido com o resultado da operação e demonstrar o número final.
+
+
+
+
+
+
+
+
+
+
