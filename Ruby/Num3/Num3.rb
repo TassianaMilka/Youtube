56b98060778b506ba55982c1ae4f8b6b490df1f7
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-09 12:18:37 -0300

    1

---
+++ Ruby/Num3/Num3.rb
@@ -0,0 +1,37 @@
+
+
+
+
+# Exer
+
+#Faça um código na finalidade para impimir o número 7, uma subtração entre 4 com 2,depois realizar  a divisão do que
+#foi imprimido com o resultado da operação e demonstrar o número final.
+
+#Declaração da variável
+num=7;
+subtracao=4-2;
+divisao=num/subtracao;
+
+
+puts  num
+
+puts subtracao
+
+puts divisao
+
+
+
+#Declaração da variável
+
+
+valorp=120.00;
+valors=20.00;
+soma=120.00+20.00;
+
+puts soma
+
+
+#Em uma tabela contém dois valores que são 120.00 e 20.00 reais com o resultado total de 140.00,mas o funcionário
+# deseja saber se retirar 20.00 reais que resultado vai obter.
+
+
