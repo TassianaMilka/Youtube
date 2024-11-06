7ec3538435e4d06d07f073f934d934f27b1f46e6
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-11-05 22:42:10 -0300

    Create Exer2-3.c

---
+++ Create Logical in Programming Language/Exer3/Exer2-3.c
@@ -0,0 +1,22 @@
+/*Início 
+Declaração de  variáveis
+Imprimir a mensagem O valor real é 20.
+Finalização*/
+
+//Início 
+#include <stdio.h>
+
+int main() {
+
+    //Declaração de  variáveis
+
+    float valor=20;
+
+    //Imprimir a mensagem
+    printf("O valor real é = R$%f",valor);
+
+    
+    return 0;
+    
+   // Finalização
+}
