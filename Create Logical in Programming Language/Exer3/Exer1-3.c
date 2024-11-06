78978a3df988bf4f09f54cd542735e345fd5aae0
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-11-05 22:41:37 -0300

    Create Exer1-3.c

---
+++ Create Logical in Programming Language/Exer3/Exer1-3.c
@@ -0,0 +1,33 @@
+/*Início 
+Declaração de variáveis
+Imprimir a mensagem Número secreto do loop
+Duas vezes imprimir o número 
+Finalização */
+
+//Início
+#include <stdio.h>
+
+int main()
+{
+    //Declaração de variáveis
+    int num=8;
+    int i;
+    //Imprimir a mensagem
+    
+    printf("Número secreto do loop\n");
+
+ 
+   //Loop 
+   
+   for(i=0;i<2;i++){
+       
+     //Duas vezes imprimir o número 
+     
+     printf("%d\n",num);
+     
+   }
+   
+  
+    return 0;
+    //Finalização
+}
