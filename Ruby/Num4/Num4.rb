6f15bd27e6221817d1f5c5bd8cdd74bedcd3c104
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-11-29 16:44:30 -0300

    1

---
+++ Ruby/Num4/Num4.rb
@@ -0,0 +1,21 @@
+
+#Em uma tabela contém dois valores que são 120.00 e 20.00 reais com o resultado total de 140.00,mas o funcionário
+# deseja saber se retirar 20.00 reais que resultado vai obter.
+
+#Declaração da variável
+
+subtracao= 140.00-20.00;
+
+#Imprimir o resultado da subtração
+
+puts subtracao;
+
+#Declaração da variável
+string = "email1270@.gmail.com";
+
+
+#Imprimir a string
+puts string;
+
+ 
+# Realiza uma string sobre uma frase de reflexão para imprimir.
