596640a9827b6aefc34b59ecd4b096177461b97d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-12-31 13:06:19 -0300

    1

---
+++ Ruby/Num5/Num5.rb
@@ -0,0 +1,17 @@
+
+# Exer
+ 
+# Realiza uma string sobre uma frase de reflexão para imprimir.
+
+#Declaração da variável
+
+string = "Nunca esqueça que sua força e habilidade pode ser maior que sua dor e dificuldade para renovar sua vida e outras.";
+
+#Imprimir a string
+
+puts string;
+
+
+#PUTS(print/Imprimir);
+#Operadores como +,-,/ e *;
+#String.
