e9f6c34a23d46a1cde8a0934675fd00bfd30dd33
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-11 15:17:58 -0300

    1

---
+++ Create Logical in Programming Language/Exer5/Exemplo3.c
@@ -0,0 +1,28 @@
+//Começo
+#include <stdio.h>
+
+int main()
+{
+    
+   //Declaração
+    int num=3;
+    
+  
+    //Validação maior o menor
+    if(num>=7){
+        
+    //Imprimir a mensagem
+    printf("\nO número %d é maior",num);
+
+        
+    }else{
+        
+    //Imprimir a mensagem
+     printf("\nO número %d é menor",num);
+
+        
+    }
+    
+    //Fim
+    return 0;
+}
