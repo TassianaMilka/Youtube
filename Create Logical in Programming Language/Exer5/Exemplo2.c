2f46eb6a8c232e22ec1539c38b810a3ae313b6f9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-11 15:17:26 -0300

    1

---
+++ Create Logical in Programming Language/Exer5/Exemplo2.c
@@ -0,0 +1,16 @@
+//Começo
+#include <stdio.h>
+
+int main()
+{
+    
+  
+    //Imprimir a mensagem
+    
+    printf("Treinamento de Lógica para aprender Linguagem de Programação");
+   
+    
+    //Fim
+    return 0;
+}
+
