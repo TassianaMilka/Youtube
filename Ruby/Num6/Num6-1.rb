e3c0499bcf35db349771b4935b81117fc6d3d611
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-18 17:40:39 -0300

    1

---
+++ Ruby/Num6/Num6-1.rb
@@ -0,0 +1,29 @@
+#Declaração string
+stringnome="Nome:Milka";
+stringidade="Idade:21 anos";
+stringcidade="Cidade:Campinas";
+
+#Imprimir os dados das string
+puts stringnome;
+puts stringidade;
+puts stringcidade;
+
+#Imprimir a mensagem
+puts"Os preços em $(dolar)";
+
+#Imprimir a mensagem 
+puts"Total";
+
+#Realizar a soma dos preços em dolar
+soma=100.00+90.00+5.00+40.00+20.00;
+
+#Imprimir a resultado da soma
+puts soma;
+#Imprimir a mensagem
+puts"Desconto";
+
+#Realizar a subtração dos preços em dolar
+subtracao=soma-100.00;
+
+#Imprimir o resultado
+puts subtracao;
