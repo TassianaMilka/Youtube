9d7219722d61a69e0f935fa99434cdcce0058f58
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-18 17:41:29 -0300

    1

---
+++ Ruby/Num6/Num6-2.rb
@@ -0,0 +1,50 @@
+#Imprimir a string da equação
+puts stringum;
+#Imprimir a mensagem
+puts"Pontenciação de 4(2)";
+#Imprimir a pontenciação
+pot=4*4;
+#Imprimir  o resultado da pontenciação
+puts pot;
+#Declrar o número(atribuição)
+num=4;
+#Imprimir a mensagem
+puts"Raiz de 16";
+#Imprimir o reultado da pontenciação
+puts num;
+#Declaração de string da equação
+stringdois="4x+2=2x-1";
+#Imprimir a string da equação
+puts stringdois;
+#Imprimir a mensagem
+puts"Número com letra de um lado e do outro lado número sem letra";
+#Declaração de string da equação
+stringtres="4x+2x=2-1";
+#Imprimir a string da equação
+puts stringtres;
+#Declaração de string da equação
+stringquatro="4x+2x";
+#Imprimir a string da equação
+puts stringquatro;
+#Realizar a soma 
+soma=4+2;
+#Imprimir a string da equação
+puts soma;
+#Declaração de string da equação
+stringquinta="2-1";
+#Imprimir a string da equação
+puts stringquinta;
+#Realizar uma subtração
+subtracao=2-1;
+#Imprimir a string da equação
+puts subtracao;
+#Declaração de string da equação
+stringsexta="6x=1";
+#Imprimir a string da equação
+puts stringsexta;
+#Imprimir a string da equação
+puts"Divisão de x=6/1";
+#Realizar a divisão
+divisao=6/1;
+#Imprimir a divisão
+puts divisao;
