36f35749a1505458bd8fdf5ad44b9fe7a494c601
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-23 17:11:34 -0300

    Create Main.java

---
+++ Java/Exerjava1/Main.java
@@ -0,0 +1,30 @@
+
+//Java contém nome de class;
+
+//Java contém tipo de variável;
+
+
+//Java contém + no System para imprimir uma mensagem o resultado;
+
+
+public class Main{//Nome classe
+    
+	public static void main(String[] args) {
+	    
+	    
+	    //Declaração de uma variável
+	    
+	    int num=9/3;
+	    
+	   
+	    //Imprimir a mensagem e o resultado
+	    
+		System.out.println("\n"+"O resultado da Divisão="+num);
+		
+		//System.out.println(num);
+		
+	}
+}
+
+
+   //Faça um código de uma soma para subtrair pelo número 2 e depois imprima o resultado.
