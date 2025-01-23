95fe08c22cc9ea7d8e151132a2ea374a8a4b0e9b
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-23 16:55:51 -0300

    1

---
+++ Create Logical in Programming Language/Exer6/HelloWorld.C#.cs
@@ -0,0 +1,23 @@
+
+using System;
+class HelloWorld {
+  static void Main() {
+      
+      
+      
+      string dados="Nome:Tassiana,País:Brasil,Área de estudo:Tecnologia,Telefone:20102-1456";
+    
+    
+   
+      Console.WriteLine("String dos Dados\n");
+      
+      
+      System.Console.WriteLine(dados);
+
+
+
+      }
+  
+  
+  
+}
