614e0e9365e7a624e21f539cf478eb7f9daa584a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-02-07 14:22:00 -0300

    1

---
+++ Ruby/Num7/Num7.rb
@@ -0,0 +1,36 @@
+
+       
+   #Declaração string
+   escolher="T";
+   mensagem_P="Hoje é dia de trabalhar";
+   mensagem_S="Hoje é dia de não trabalhar";
+   mensagem_T="Não encontrado no sistema";
+       
+    
+    
+   #Condição(validação)
+    
+   puts"\n";
+   
+   if(escolher=="P")
+     #Imprimir a string
+     puts mensagem_P;
+   
+   
+   elsif(escolher=="S")
+      #Imprimir a string
+      puts mensagem_S;
+      
+      
+    else
+        #Imprimir a string
+        puts mensagem_T;
+        
+   #Finalização da execução 
+   end
+   
+   #Exer
+  
+  #Faça um código destinado para uma estrutura de codição(validação) que possa saber se o resultado da divisão com 
+  #subtração é maior que 10,menor ou igual e finaliza com uma mensagem.
+
