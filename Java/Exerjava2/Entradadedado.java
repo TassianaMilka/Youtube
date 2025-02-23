e7ce7ee143c9dee5b48388a1d16d39ec5240c35c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-02-23 02:10:40 -0300

    1

---
+++ Java/Exerjava2/Entradadedado.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+// Entrada de dado e saída;
+
+//Diferente forma de declaração de variável
+
+
+public class Main {
+    public static void main(String[] args) {
+
+       //Destinado para entrada de dado e saída de dado
+
+        Scanner ler = new Scanner(System.in);
+
+       //Declaração de variável
+
+        int num;
+
+        System.out.println("\n"+"\n");
+
+        //Entrada de dado
+        System.out.println("Informe o número");
+        //Saída de dado
+        num=ler.nextInt();
+
+        //Imprimir a mensagem e o número informado pelo usuário
+
+        System.out.println("O número informado pelo usuário é="+num);
+    }
+}
+
+
+
+       /* Double valor =200.00;
+
+        System.out.println("\n"+"\n");
+
+        System.out.println("O valor informado pelo usuário é="+valor);*/
+
+
+   //Realiza um código destinado para saber a quantidade exata que um cliente contém na sua conta do banco.Lembrando que possuí R$200,00 e adicionou
+   //R$4000 e retirou R$100,00.Qual é a quantidade final?
