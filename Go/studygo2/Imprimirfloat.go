a544b42742b1450ea55c4e6f7590f38ebba5a2cf
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-02-24 18:11:12 -0300

    1

---
+++ Go/studygo2/Imprimirfloat.go
@@ -0,0 +1,23 @@
+
+package main //Ponto de entrada 
+
+import "fmt" //format(formatação para imprimir o println)
+
+
+func main() { //Função prinicipal do programa
+
+    //Declaração de variáveis
+    
+    var altura float32=1.80;
+    var valor float64=400;
+
+    //Imprimir a mensagem no console(print) e altura
+    
+    fmt.Println("Altura é=",altura);
+    
+    //Imprimir a mensagem no console(print) e valor 
+    
+    fmt.Println("Valor é R$=",valor);
+
+
+}
