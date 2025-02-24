b25607c6a673a133aed10a9905c29dada5beddde
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-02-24 18:10:31 -0300

    1

---
+++ Go/studygo2/Calnovo1.go
@@ -0,0 +1,20 @@
+
+package main //Ponto de entrada 
+
+import "fmt" //format(formatação para imprimir o println)
+
+
+func main() { //Função prinicipal do programa
+
+    //Declaração de variável e realização de cálculo
+    
+    var cal int=5+5-2;
+   
+    //Imprimir a mensagem no console(print) e o resultado
+    //do cálculo
+    
+    fmt.Println("O resultado é=",cal);
+
+
+
+}
