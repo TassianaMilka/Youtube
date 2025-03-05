614fabb72cd3bfd29b0f3d7bb2b371d56049c76d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-05 16:25:05 -0300

    1

---
+++ Go/studygo3/Letrachar.go
@@ -0,0 +1,18 @@
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+
+func main() { //Função prinicipal do programa
+
+    //Declaração de variável
+
+    letra:="T";
+    
+    //Imprimir a letra no console(print)
+    
+    fmt.Println(letra);
+
+
+}
