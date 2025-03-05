0b0589a782f33b39f6a6cf432172af11681d3331
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-05 16:20:28 -0300

    1

---
+++ Go/studygo3/Mensagemstring.go
@@ -0,0 +1,18 @@
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+
+func main() { //Função prinicipal do programa
+
+    //Declaração de variável
+
+    mensagem:="São Paulo,Rio de Janeiro,Minas Gerais e Espírito Santo são da região sudeste do Brasil";
+    
+
+    //Imprimir a mensagem no console(print)
+    
+    fmt.Println(mensagem);
+
+
+}
