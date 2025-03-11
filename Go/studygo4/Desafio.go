e8f12b5c0ecd37d4e9f049ce8cc587aabf9da487
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 13:56:30 -0300

    1

---
+++ Go/studygo4/Desafio.go
@@ -0,0 +1,28 @@
+//Desafio
+
+//Criar um código que a sua principal funcionalidade é imprimir
+//um nome de um produto e que possa realizar um cálculo de 100 convertido para
+// 0,1(juro)de uma subtração de R$900.00 para retirar R$700.00.
+//Conversão(100=0,1).
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() { //Função prinicipal do programa
+
+     //Declaração das variáveis
+
+     produtoNome:="Celular";
+    
+     var cal float64=900.00-700.00*0.1;
+    
+     //Imprimir o nome do produto no console(print)
+     fmt.Println(produtoNome);
+    
+     //Imprimir o nome do produto no console(print)
+    
+     fmt.Println(cal);
+    
+    
+}
