17d9a62854a6d2461c7e071ad03d60d6caa0c96c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 13:56:03 -0300

    1

---
+++ Go/studygo4/Stringandchar.go
@@ -0,0 +1,18 @@
+
+func main() { //Função prinicipal do programa
+
+    //Declaração de variáveis
+
+    mensagem:="São Paulo,Rio de Janeiro,Minas Gerais e Espírito Santo são da região sudeste do Brasil";
+    letra:="T";
+
+    //Imprimir a mensagem no console(print)
+    
+    fmt.Println(mensagem);
+
+   //Imprimir a letra no console(print)
+
+    fmt.Println(letra);
+
+
+}
