0a4a00a971be5d3fbe24652a5654bdb7082885d0
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-18 13:14:03 -0300

    1

---
+++ Go/studygo5/studygo5.go
@@ -0,0 +1,41 @@
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() { //Função prinicipal do programa 
+
+     //Declaração da variável
+
+       var dia_consulta int=18;
+    
+
+     //Estrutura de condição(validação)
+
+     if dia_consulta==18{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Hoje é o dia da consulta por volta das 16:00!");
+
+         
+     }else if dia_consulta==19{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Hoje não é dia da consulta!");
+  
+        
+    }else{
+
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Não encontrado o dia da consulta no sistema!");
+  
+       
+   }
+    
+   
+    
+    
+    
+    
+}
