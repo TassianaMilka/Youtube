faebb13022c6d8e035185516cf402b89c031b7c6
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-24 17:42:21 -0300

    1

---
+++ Go/studygo6/studygo6.go
@@ -0,0 +1,38 @@
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+ 
+func main() { //Função prinicipal do programa 
+
+     //Declaração da variável
+
+       sigla := "NORC";
+
+    //Norte,Nordeste,Centro-Oeste,Sudeste e Sul
+
+   // NORC
+   // SDES
+
+     //Estrutura de condição(validação)
+
+     if sigla=="NORC"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("A passagem comprada é para Região Norte,Nordeste ou Centro-Oeste do Brasil!");
+
+         
+     }else if sigla=="SDES"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("A passagem comprada é para Região Sudeste e Sul do Brasil!");
+  
+        
+    }else{
+
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Obteve um erro no sistema!");
+  
+       
+   }
+    
