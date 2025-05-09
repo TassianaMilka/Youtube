7d5a474b1260abc20a65afe10844051acef42833
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-05-09 17:09:37 -0300

    1

---
+++ Go/studygo7/Sigla.go
@@ -0,0 +1,33 @@
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+ 
+func main() { //Função prinicipal do programa 
+
+     //Declaração da variável 
+
+       sigla := "B";
+
+     //Estrutura de condição(validação) com(ou ||)
+
+     if sigla=="A" || sigla=="B"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("A sala (a e b) obteve a maior média de pontuação!");
+
+         
+    }else if sigla=="C" || sigla=="D"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("A sala (c e d) obteve uma regular média de pontuação!");
+  
+
+    }else{
+
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Obteve um erro no sistema!");
+  
+       
+   }
+    }
