b38a85d8fefdc07f8e388d6428fd4126d8cbbdce
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-05-09 17:12:33 -0300

    1

---
+++ Go/studygo7/studygo7-2.go
@@ -0,0 +1,19 @@
+func main() { //Função prinicipal do programa 
+
+     //Declaração da variável 
+
+      var valor float64=300;
+  
+
+     //Estrutura de condição(validação) com(e &&)
+
+     if (valor>=200 && valor<=500){
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("O valor está entre R$200.00 até R$500.00!");
+
+    }else{
+
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Obteve um erro no sistema!");
