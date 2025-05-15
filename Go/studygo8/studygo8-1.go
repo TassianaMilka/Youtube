9466660af239f1b7ba6c14137e77ad5c864d20a6
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-05-15 13:16:24 -0300

    1

--- Go/studygo8/studygo8-1.go
+++ Go/studygo8/studygo8-1.go
@@ -1,1 +1,44 @@
+package main //Ponto de entrada  
 
+import "fmt" //format(formatação para imprimir o println)
+ 
+func main() { //Função prinicipal do programa 
+
+     //Declaração das variáveis 
+      
+       sigla := "B";
+    
+       var valor float64=300;
+
+     //Estrutura de condição(validação) com(ou ||)
+
+     if sigla=="A" || sigla=="B"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("A sala (a e b) obteve a maior média de pontuação!");
+
+         
+    } 
+    if sigla=="C" || sigla=="D"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("A sala (c e d) obteve uma regular média de pontuação!");
+
+}
+
+     //Estrutura de condição(validação) com(e &&)
+
+     if (valor>=200 && valor<=500){
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("O valor está entre R$200.00 até R$500.00!");
+
+    }else{
+
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Obteve um erro no sistema!");
+     
+        }   
+   
+    }
