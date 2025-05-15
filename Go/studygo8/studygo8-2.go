48f14ea06a29514c34d9f15858abb0e13ad3ab84
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-05-15 13:17:05 -0300

    1

---
+++ Go/studygo8/studygo8-2.go
@@ -0,0 +1,17 @@
+package main // Ponto de entrada
+
+import "fmt" // Importando o pacote fmt para impressão
+
+func main() { // Função principal do programa
+
+    // Declaração de variável 
+    var i int 
+
+    // Loop(Estrutura de repetição)
+    for i = 0; i < 2; i++ {
+        // Imprimir a mensagem no console(print)
+        fmt.Println("Estudando Linguagem de Programação Go!")
+        fmt.Println(i)
+     
+    }
+}
