ed50b1a5ad667d2b11c16426ad3b70ab06a6e8a4
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-10 21:27:35 -0300

    1

---
+++ Go/studygo9/studygo9.go
@@ -0,0 +1,20 @@
+package main // Ponto de entrada
+
+
+import "fmt" // Importando o pacote fmt para impressão
+
+
+func main() {  // Função principal do programa
+    
+    //Array de inteiros com tamanho 10
+    var arr [10]int 
+    
+    //Atribuindo valor ao primeiro elemento
+    arr[9] = 10  
+    
+    //Imprimir os números inteiros de vetor do tamanho 10
+    fmt.Println("O Vetor de Inteiros\n")
+    
+    fmt.Println(arr) 
+
+}
