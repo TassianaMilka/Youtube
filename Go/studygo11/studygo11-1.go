b0c146102fb2607ec358bcf7140a517e2e56084e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-23 19:53:44 -0300

    1

---
+++ Go/studygo11/studygo11-1.go
@@ -0,0 +1,22 @@
+// Realiza um código destinado para acessar uma rede social
+//pelo nome e uma letra de identificação do usuário.
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+
+func main() { //Função principal do programa
+
+    //Declaração de variáveis
+
+    nome:="Tassiana";
+    letra:="u";
+
+    //Imprimir a mensagem no console(print)
+    
+    fmt.Println("o nome do usuário:",nome);
+    fmt.Println("A letra de identificação:",letra);
+
+
+}
