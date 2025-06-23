c268589fbe8c9d6616f09aa1e5cf4c8cc554a144
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-23 19:54:07 -0300

    1

---
+++ Go/studygo11/studygo11-2.go
@@ -0,0 +1,29 @@
+//Desafio
+
+//Realiza uma soma de dois números que possa dividir com 
+//outro número,descrição da frase do resultado da soma 
+//e divisão.
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+
+func main() { //Função principal do programa 
+
+     //Declaração das variáveis
+
+    mensagem:="O resultado da soma com divisão";
+    
+     var calculo float64=900+300/30;
+    
+     //Imprimir o nome do produto no console(print)
+     fmt.Println(mensagem);
+    
+     //Imprimir o nome do produto no console(print)
+    
+     fmt.Println(calculo);
+    
+    
+}
+
+
