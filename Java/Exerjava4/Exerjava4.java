a3b17a22bb81c7c4b6f1365d2822e0e989ad0f32
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-30 17:05:00 -0300

    1

--- Java/Exerjava4/Exerjava4.java
+++ Java/Exerjava4/Exerjava4.java
@@ -1,1 +1,51 @@
 
+import java.util.Scanner;
+
+
+
+
+   //String.
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        //String
+
+        //A seguir observa esse código de string:
+
+      /* //Declaração de variável
+        String frase = "Tecnologia é para todos!";
+
+        System.out.println("\n"+"\n");
+
+        //Imprimir a frase
+        System.out.println(frase); */
+
+
+        //Realiza de outra forma esse código com entrada de dado e saída de dado.
+
+       //Destinado para entrada de dado e saída de dado
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("\n"+"\n");
+
+        //Entrada de dado
+        System.out.println("Informe  a frase\n");
+        //Saída de dado
+        String frase=ler.nextLine();
+
+        System.out.println("\n"+"\n");
+
+        //Imprimir o cálcaulo da mensagem
+       System.out.println(frase);
+
+
+
+
+    }
+}
