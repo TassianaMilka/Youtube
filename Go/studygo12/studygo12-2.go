518eb1d1efe2d356927f61fc3be6093407705a92
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-30 17:11:33 -0300

    1

---
+++ Go/studygo12/studygo12-2.go
@@ -0,0 +1,24 @@
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+ 
+func main() { //Função principal do programa 
+
+     //Declaração das variáveis
+
+       dia_semana := "dia";
+       frase:="Na rua1 o lixo é coletado de Segunda-feira,Na rua2 o lixo é coletado na Terça-feira,Na rua3 o lixo é coletado na Quarta-feira."
+
+
+     //Estrutura de condição(validação)
+
+     if dia_semana=="dia"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println(frase);
+
+         
+     }
+  
+
+}
