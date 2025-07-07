85221be4df077fd9735b1b72e09fa42a217a9f3d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-07-07 17:32:06 -0300

    1

--- Go/studygo13/studygo13-1.go
+++ Go/studygo13/studygo13-1.go
@@ -1,1 +1,36 @@
 
+//Realiza um código para demonstrar uma string de letras com números
+//utilizando operador ou(||) e identifica com mensagem que foi
+//encontrado no Banco de dado ou não foi encontrado.
+
+
+
+package main //Ponto de entrada  
+
+import "fmt" //format(formatação para imprimir o println)
+ 
+func main() { //Função principal do programa 
+
+     //Declaração da variável 
+
+       cod_identificao := "IA589n"; 
+ 
+     //Estrutura de condição(validação) com(ou ||)
+
+
+         
+     if cod_identificao=="IA589n" || cod_identificao=="tr1Ubq"{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Encontrado o código no Banco de dado!");
+  
+
+    }else{
+
+     //Imprimir a mensagem no console(print)
+     fmt.Println("Obteve um erro na execução no sistema ou não encontrado!");
+  
+       
+       }
+    }
+
